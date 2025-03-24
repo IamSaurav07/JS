755f0b9678a8ac9f5e652017229195f9dd4d1005
SAURAV YADAV <91652627+IamSaurav07@users.noreply.github.com>
2025-03-24 08:21:12 +0530

    Add files via upload

---
+++ 01-variable.js
@@ -0,0 +1,16 @@
+const accountID = 17353           //constant
+let accEmail = "sau@gfbds"        //normal variable with initialization
+var accPassword = "nbs2ue3"       //initially it's used it have a problem of a scope....now never used
+accCity = "Jaipur"                //not recommended but it's possible
+
+let accState                       //if not initialisec will return undefined
+
+accEmail = "jsudh@gmail"
+accPassword = "6w8h66e"
+accCity = "Delhi"
+
+console.log(accountID);
+
+console.table([accountID, accPassword, accEmail, accCity])   // It will print all the output in a table format
+
+
---
+++ 2_Variable.js
@@ -0,0 +1,16 @@
+"use strict"   //treat all JS code as of newer version
+
+//alert(3+3) cannot be used here, but can be used in browser as JS engine is in browser
+
+//console.log("sggd"); console.log(554);
+let name = "saurav"
+let age = 23
+let great = true;   //boolean
+
+//undefined: value not given
+//null : void not present but not intensialy kind of object
+//symbol : used for uniqness
+
+console.log(typeof name)            //returns string
+console.log(typeof null)            //returns object
+console.log(typeof undefined)       //returns undefined
